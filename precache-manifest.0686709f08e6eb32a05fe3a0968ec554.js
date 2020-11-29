4630b9a41f47fed8b14e6990aad67f5b5564ac7b
Stanislav Labzyuk <stanislav.labzyuk@gmail.com>
2020-11-29 17:04:01 +0700

    Updates

---
+++ precache-manifest.0686709f08e6eb32a05fe3a0968ec554.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630ac05d3eb8051b1b21006f5f9efa6e",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "74509fb6ff79aa161df3",
+    "url": "/app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7b706a311c8c209b3fb2",
+    "url": "/app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "74509fb6ff79aa161df3",
+    "url": "/app/static/js/2.37432646.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/app/static/js/2.37432646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b706a311c8c209b3fb2",
+    "url": "/app/static/js/main.04bdb515.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/app/static/media/persik.4e1ec840.png"
+  }
+]);
